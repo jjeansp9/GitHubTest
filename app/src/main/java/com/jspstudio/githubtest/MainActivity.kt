75ff5190d2f51af134db50ffeb70bcf4e728cc2c
jjeansp9 <jjeansp9@gmail.com>
2022-11-02 15:22:59 +0900

    make btn view

--- app/src/main/java/com/jspstudio/githubtest/MainActivity.kt
+++ app/src/main/java/com/jspstudio/githubtest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.jspstudio.githubtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn:Button = findViewById(R.id.btn)
+        btn.setOnClickListener { Toast.makeText(this, "click!", Toast.LENGTH_SHORT).show() }
     }
 }
