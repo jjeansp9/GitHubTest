4174055a760080f482ef397efafdd07921a22925
jjeansp9 <jjeansp9@gmail.com>
2022-11-02 15:39:13 +0900

    add textview

--- app/src/main/java/com/jspstudio/githubtest/MainActivity.kt
+++ app/src/main/java/com/jspstudio/githubtest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jspstudio.githubtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         val btn:Button = findViewById(R.id.btn)
         btn.setOnClickListener { Toast.makeText(this, "click!", Toast.LENGTH_SHORT).show() }
+
+        val tv:TextView = findViewById(R.id.tv)
+        tv.text = "nice to meet you"
     }
 }
